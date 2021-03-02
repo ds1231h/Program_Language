b9949556fceca7c14654d1436fe4b8d331fc7e58
穆沛 <SENSETIME\mupei@cn0614007310l.domain.sensetime.com>
2021-03-02 11:28:35 +0800

    C to shell

---
+++ c2shell.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <memory.h>
+
+int main() {
+  for (int i = 0; i < 10; i++) {
+    char *echo = (char*)malloc(6 * sizeof(char));
+    sprintf(echo, "echo %d", i);
+    system(echo);
+  }
+  return 0;
+}
